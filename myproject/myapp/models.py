4592749b999010d089fc12f3b542910767417cf3
farukerdem <farukomererdem2003@gmail.com>
2024-10-20 02:11:05 +0300

    boolean fix

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Name(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    clicked = models.BooleanField(_(""))
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
