861b8080c862bab98edc49eecc0f0fd951ce4f94
farukerdem <farukomererdem2003@gmail.com>
2024-10-20 03:18:17 +0300

    mysql

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Name(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+    count = models.IntegerField()
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'phishing',
+        'USER': 'phisher',
+        'PASSWORD': 'user_password',
+        'HOST': 'db',  # veya başka bir IP
+        'PORT': '3306',  # varsayılan MariaDB portu
     }
 }
 
